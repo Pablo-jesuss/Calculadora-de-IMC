2be3c641b28f02a8b42073674092b6860429db49
Pablo-jesuss <pablo.programador26@gmail.com>
2024-03-14 15:25:23 -0300

    imcTable

--- imc_Calc/src/components/ImcTable.jsx
+++ imc_Calc/src/components/ImcTable.jsx
@@ -1,7 +1,20 @@
 import "./ImcTable.css";
 
-const ImcTable = () => {
-  return <div></div>;
+const ImcTable = ({ data }) => {
+  return (
+    <div id="result-container">
+      <p id="imc-number">Seu Imc:</p>
+      <p id="imc-info">Situação atual</p>
+      <h3>Confira as classificações</h3>
+      <div id="imc-table">
+        <div className="table-header">
+          <h4>IMC</h4>
+          <h4>Classificação</h4>
+          <h4>Obesidade</h4>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ImcTable;
